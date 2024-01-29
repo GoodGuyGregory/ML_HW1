788962b364400dd1b04b3f97209273eb0142b2da
GoodGuyGregory <greg.witt625@gmail.com>
2024-01-29 11:44:06 -0500

    adds initial activation function and Perceptron_network class

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import numpy as np
+import pandas as pd
+
+def Perceptron_Network():
+    def __init__(self, input_size, hidden_size, learning_rate):
+        self.weights = np.random.uniform(low=-0.5, high=0.5, size=(input_size,))
+        self.bias = np.random.uniform(low=-0.5, high=0.5)
+        self.learning_rate = learning_rate
+
+
+    def forward():
+        pass
+
+    def backward():
+        pass
+
+    # activation function for network
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
 
 def main():
-    print("Works")
+
 
 
 main()
