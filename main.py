5c75e6dfd85f4b1a16d6f774d007917046d145a7
GoodGuyGregory <greg.witt625@gmail.com>
2024-01-31 10:36:21 -0500

    initiated backpropagation process

--- main.py
+++ main.py
@@ -3,74 +3,120 @@ import numpy as np
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
+import copy
+
 
 
 # TODO: Increase input vector capacity to feature a bias feature.
 # Process forward propagation with the sigmoid function swashing
 # create a backward method that will handle weight updates based on the algorithm in class.
+class Perceptron_Optimizer:
+
+    def backward(self, perceptronLayer, truthVector, input):
+
+        #  determine accuracy
+        perceptronLayer.updateAccuracy(truthVector)
+
+        # if no backprop done before
+        if perceptronLayer.prevOutputWeightsDelta is None:
+            # compute the error at the output:
+            inputActivationsDiff = 1 - perceptronLayer.outputLogits
+            truthVectorDiff = truthVector - perceptronLayer.outputLogits
+            # based on the equation from the notes.
+            deltaKError = perceptronLayer.outputLogits * np.dot(inputActivationsDiff, truthVectorDiff)
+
+            #  compute the error for the hidden layer
+
+            hiddenError = 1.0 - perceptronLayer.hiddenLogits
+            hiddenDelta = perceptronLayer.hiddenLogits * hiddenError * np.dot(perceptronLayer.hWeights, deltaKError)
+
+            #  no momentum is required as there is no previous weight to multiply by
+            # update the hidden to output weights.
+            perceptronLayer.hweights = perceptronLayer.eta * np.outer(perceptronLayer.hiddenLogits.T, deltaKError)
+            # update the input to the hidden weights
+            perceptronLayer.weights = perceptronLayer.eta * np.outer(input, hiddenDelta)
+            #      set the previous weights for later momentum calculations:
+            perceptronLayer.prevHiddenWeightDelta = perceptronLayer.hWeights
+            perceptronLayer.prevOutputWeightsDelta = perceptronLayer.weights
+
+        else:
+            # compute the error at the output:
+            inputActivationsDiff = 1 - perceptronLayer.outputLogits
+            truthVectorDiff = truthVector - perceptronLayer.outputLogits
+            # based on the equation from the notes.
+            deltaKError = perceptronLayer.outputLogits * np.dot(inputActivationsDiff, truthVectorDiff)
+
+            #  compute the error for the hidden layer
+
+            hiddenError = 1.0 - perceptronLayer.hiddenLogits
+            hiddenDelta = perceptronLayer.hiddenLogits * hiddenError * np.dot(perceptronLayer.hWeights, deltaKError)
+
+            # hidden-to-output weights
+            perceptronLayer.hweights = perceptronLayer.eta * np.dot(deltaKError, perceptronLayer.hiddenLogits.T) + (perceptronLayer.momentum * perceptronLayer.prevHiddenWeightDelta)
+            # input-to-hidden
+            perceptronLayer.weights = perceptronLayer.eta * np.dot(hiddenDelta, input) + (perceptronLayer.momentum * perceptronLayer.prevOutputWeightsDelta)
+
+            # set new previous for the next cycle.
+            perceptronLayer.prevHiddenWeightDelta = perceptronLayer.hWeights
+            perceptronLayer.prevOutputWeightsDelta = perceptronLayer.weights
+
+
+    #  produces on hot encoded sigmoid vector
+    def convertTruth(self, Y):
+        groundTruth = np.zeros(10,dtype='float')
+        groundTruth[Y] = 0.9
+        return groundTruth
 
 
 
 class Perceptron_Layer:
-    def __init__(self, n_inputs, n_neurons, learning_rate):
+    def __init__(self, n_inputs, n_neurons, h_neurons, learning_rate, momentum):
+        self.eta = learning_rate
+        self.momentum = momentum
+
+        #  standard input weights
+        self.weights = np.random.uniform(low=-0.5, high=0.5, size=(n_inputs + 1, h_neurons))
+        # add a bias term inside each weight vector
+        self.weights[-1] = 1
+
+        # hidden weights.
+        self.hWeights = np.random.uniform(low=-0.5, high=0.5, size=(h_neurons, n_neurons))
+        # add a bias term inside each weight vector
+        self.hWeights[-1] = 1
+
+        self.hiddenLogits = []
+
+        self.outputLogits = []
+
+        #  Place holders for now.
+        self.prevHiddenWeightDelta = None
+        self.prevOutputWeightsDelta = None
+
         self.accuracy = 0.0
         self.accurateCount = 0
         self.incorrect = 0
         self.inputSize = 0
-        self.weights = np.random.uniform(low=-0.5, high=0.5, size=(n_inputs,  n_neurons))
-        # add a bias term inside each weight vector
-        self.weights = np.insert(self.weights, self.weights.shape[1], axis=1)
-        # 1 x n_neurons matrix
-        self.biases = np.ones((1, n_neurons))
-        self.eta = learning_rate
-        self.grounds = []
-        self.potentials = []
 
-    #  takes a single input
+    def sigmoid(self, x):
+        x *= -1
+        return 1 / (1 + np.exp(x))
 
     #  append the bias.
-    def forward(self, input, target, adjustment):
+    def forward(self, input):
         #  convert the target to a vector representation
 
-        # inputs [1x784] dot [784 x 10] + [1x10]
-        activationPotential = np.dot(input, self.weights)
-        # perceptron results
-        # [1 x 10] result matrix
+        # inputs [785x1] dot [785 x N] + [1xN]
+        layerOneLogits = np.dot(input.T, self.weights)
+        self.hiddenLogits = self.sigmoid(layerOneLogits)
+        # hWeights [10 x 50] dot [1x50]
+        hiddenLayerLogits = np.dot(self.hWeights.T, self.hiddenLogits.T) + 1
+        # activation function
+        self.outputLogits = self.sigmoid(hiddenLayerLogits)
 
-        prediction = np.argmax(activationPotential)
-        #
-        activationPotential = np.where(activationPotential > 0, 1, 0)
-        #  create a truthVector t^i per equation for weight adjustment
-        truthVector = np.zeros(10, dtype="int")
-        truthVector[target] = 1
-
-        self.grounds.append(truthVector.argmax())
-        self.potentials.append(activationPotential.argmax())
-
-        if target == prediction:
-            self.accurateCount += 1
-            self.determineAccuracy()
-        else:
-            self.incorrect += 1
-            error = []
-            for i in range(len(truthVector)):
-                error.append(truthVector[i]-activationPotential[i])
-
-            np.array(error)
-
-            #  for HW1 algo... single batch input... get the single array input and or access weights for tuning...
-            if adjustment:
-                #  threshold check
-                #  contains the binary classification from potential fire
-                self.weights = self.weights + self.eta * np.outer(error, input).T
-
-
-                #  update weights
 
         self.inputSize += 1
 
-    def sigmoid(x):
-        return 1 / (1 + np.exp(-x))
+
 
     def displayAccuracy(self):
         if self.inputSize == 0:
@@ -80,11 +126,9 @@ class Perceptron_Layer:
             self.accuracy = round((self.accurateCount / self.inputSize) * 100, 2)
             print("Accuracy: " + str(self.accuracy) + "%")
 
-    def determineAccuracy(self):
-        if self.inputSize == 0:
-            self.accuracy = 0.0
-        else:
-            self.accuracy = round((self.accurateCount / self.inputSize) * 100, 2)
+    def updateAccuracy(self, truthVector):
+        if np.argmax(truthVector) == np.argmax(self.outputLogits):
+            self.accurateCount += 1
 
     def calculateConfusionMatrix(self):
         cm = confusion_matrix(self.grounds, self.potentials)
@@ -94,7 +138,7 @@ class Perceptron_Layer:
         print("Confusion Matrix:")
         print(cm)
 
-    def plotConfusionMatrix(self, cm, testType ):
+    def plotConfusionMatrix(self, cm, testType):
         plt.figure(figsize=(8, 6))
         sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", cbar=False,
                     xticklabels=np.arange(10), yticklabels=np.arange(10))
@@ -120,7 +164,7 @@ class Perceptron_Layer:
         plt.grid(True)
         plt.show()
 
-    def plotAccuracyOverTestTrainingEpochs(self,trainAccuracies, testAccuracies, epochs):
+    def plotAccuracyOverTestTrainingEpochs(self, trainAccuracies, testAccuracies, epochs):
         plt.figure(figsize=(8, 6))
         plt.plot(trainAccuracies, marker='o')
         plt.plot(testAccuracies, marker='o')
@@ -142,28 +186,35 @@ class Perceptron_Layer:
         plt.grid(True)
         plt.show()
 
+
 def getTrainingLabels(dataFrame):
     groundTruthLables = dataFrame['label'].to_numpy()
     # drop the labels of the data and then divide each array
     return groundTruthLables
 
+
 def getTrainingInputs(dataFrame):
+
+    dataFrame['bias'] = 255
     inputs = dataFrame.drop("label", axis=1).to_numpy()
     # divide each value vector by 255...
     normalizedInputs = inputs / 255
     return normalizedInputs
+
 
 def shuffleTrainData():
     mnist_data = pd.read_csv("mnist_train.csv")
     # shuffle the contents of the dataset for training
     #  the prevents the perceptrons from being trained on ordering of the input data.
     shuffledMnistData = mnist_data.sample(frac=1)
-    return  shuffledMnistData
+    return shuffledMnistData
+
 
 def getTesingLabels(dataFrame):
     groundTruthTestLables = dataFrame['label'].to_numpy()
     # drop the labels of the data and then divide each array
     return groundTruthTestLables
+
 
 def getTestingInputs(dataFrame):
     inputs = dataFrame.drop("label", axis=1).to_numpy()
@@ -171,89 +222,47 @@ def getTestingInputs(dataFrame):
     normalizedTestInputs = inputs / 255
     return normalizedTestInputs
 
+
 def shuffleTestData():
     mnist_data = pd.read_csv("mnist_test.csv")
     # shuffle the contents of the dataset for training
     #  the prevents the perceptrons from being trained on ordering of the input data.
     shuffledMnistTestData = mnist_data.sample(frac=1)
-    return  shuffledMnistTestData
+    return shuffledMnistTestData
+
 
 # input 785 inputs within 10 perceptrons each input is a vector of 785 ie 28x28+1 (+1 for bias)
 
 def main():
     # read in the MNIST data:
+
     normalizedInputs = shuffleTrainData()
-    y = getTrainingLabels(normalizedInputs)
-    X = getTrainingInputs(normalizedInputs)
-    # normalizedInputs = normalizedInputs.reshape((-1, 28, 28))
-    # confirms 28 x 28
-    # print(normalizedInputs.shape)
-    # print(normalizedInputs[0])
+    batch = 0
+    #  add loop to control epoch count
+    perceptronLayer = Perceptron_Layer(784, 10,20, 0.1, 0.9)
+    while batch < len(normalizedInputs):
+        print("batch:" +  str(batch))
+        Y = getTrainingLabels(normalizedInputs)
+        x = getTrainingInputs(normalizedInputs)
 
-    #  Model #1 - Learning Rate 0.1
-    perceptronModel = Perceptron_Layer(784, 10, 0.001)
+        #  forward pass through the connected input layer and connected layer.
 
-    #  single input iteration for the batch size of training being 1
-    #  initial test run...
-    print("Epoch: 0")
-    print('=============================')
-    #  Training Cycles:
-    # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    accuracy_over_initial_epoch = []
-    for d in range(len(normalizedInputs)):
-        digit = X[d]
-        truth = y[d]
-        perceptronModel.forward(digit, truth, False)
-    perceptronModel.clearAccuracy()
-    #  eta is 0.1%
-    epochModelCount = 1
-    # eta 0.1
-    print("Training on learning rate at 0.01")
-    training_accuracy_over_epoch_w_eta = []
-    testing_accuracy_over_epoch_w_eta = []
-    while epochModelCount <= 70:
-        normalizedInputs = shuffleTrainData()
-        yTrain = getTrainingLabels(normalizedInputs)
-        XTrain = getTrainingInputs(normalizedInputs)
-        for b in range((len(normalizedInputs))):
-            digit = X[b]
-            truth = y[b]
-            perceptronModel.forward(digit, truth, True)
-        training_accuracy_over_epoch_w_eta.append(perceptronModel.accuracy)
-        # run model against the test set.
-        normalizedTestInputs = shuffleTestData()
-        yTest = getTesingLabels(normalizedTestInputs)
-        XTest = getTestingInputs(normalizedTestInputs)
-        for b in range((len(normalizedTestInputs))):
-            digit = XTest[b]
-            truth = yTest[b]
-            perceptronModel.forward(digit, truth, True)
-        testing_accuracy_over_epoch_w_eta.append(perceptronModel.accuracy)
-        epochModelCount += 1
-    #  clean up for 0.1 eta
-    confusion_matrix_result = perceptronModel.calculateConfusionMatrix()
-    perceptronModel.plotAccuracyOverTestTrainingEpochs(training_accuracy_over_epoch_w_eta, testing_accuracy_over_epoch_w_eta, epochModelCount)
-    perceptronModel.clearAccuracy()
+        perceptronLayer.forward(x[batch])
 
-    final_test_accuracy_over_epoch_w_eta = []
+        perceptronOptimizer = Perceptron_Optimizer()
+        #  convert truth value
+        groundTruth = perceptronOptimizer.convertTruth(Y[batch])
 
-    normalizedTestInputs = shuffleTestData()
-    yTest = getTesingLabels(normalizedTestInputs)
-    XTest = getTestingInputs(normalizedTestInputs)
-    for b in range((len(normalizedTestInputs))):
-        digit = XTest[b]
-        truth = yTest[b]
-        perceptronModel.forward(digit, truth, True)
-        final_test_accuracy_over_epoch_w_eta.append(perceptronModel.accuracy)
-    #  clean up for 0.1 eta
-    confusion_matrix_result = perceptronModel.calculateConfusionMatrix()
-    perceptronModel.displayConfusionMatrix(confusion_matrix_result)
-    # Plot confusion matrix heatmap
-    perceptronModel.plotConfusionMatrix(confusion_matrix_result, "Test Evaluation From Trained Model at Eta 0.01")
-    perceptronModel.displayAccuracy()
-    perceptronModel.plotTestAccuracySingleEpoch(final_test_accuracy_over_epoch_w_eta)
-    perceptronModel.clearAccuracy()
+        #  backward pass will update the current neuron
+        #  update it's weights based on error.
+        perceptronOptimizer.backward(perceptronLayer, groundTruth, x[batch])
+
+        batch += 1
+        if batch % 5 == 0:
+            perceptronLayer.displayAccuracy()
+
+    print("done training")
+
 
 
 main()
-
